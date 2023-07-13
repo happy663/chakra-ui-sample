713cdf921e9e358348cb7c293663c459ac309a91
happy663 <tatu2425@gmail.com>
2023-07-13 18:51:25 +0900

    feat: display

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,53 @@
-// import './App.css'
+import { Box, Flex, HStack, Image } from "@chakra-ui/react";
 
 function App() {
-
   return (
-    <>       
-        main
+    <>
+      <Flex direction={"column"} h={"100%"} alignItems={"center"}>
+        <Box overflowX="scroll" whiteSpace="nowrap">
+          <HStack spacing="25px">
+            <Image
+              borderRadius={"20px"}
+              boxSize="75px"
+              src="https://bit.ly/dan-abramov"
+              alt="Dan Abramov"
+            />
+            <Image
+              borderRadius={"20px"}
+              boxSize="75px"
+              src="https://bit.ly/dan-abramov"
+              alt="Dan Abramov"
+            />
+            <Image
+              borderRadius={"20px"}
+              boxSize="75px"
+              src="https://bit.ly/dan-abramov"
+              alt="Dan Abramov"
+            />
+            <Image
+              borderRadius={"20px"}
+              boxSize="75px"
+              src="https://bit.ly/dan-abramov"
+              alt="Dan Abramov"
+            />
+            <Image
+              borderRadius={"20px"}
+              boxSize="75px"
+              src="https://bit.ly/dan-abramov"
+              alt="Dan Abramov"
+            />
+            <Image
+              borderRadius={"20px"}
+              boxSize="75px"
+              src="https://bit.ly/dan-abramov"
+              alt="Dan Abramov"
+            />
+          </HStack>
+        </Box>
+        <Box color={"white"} flexGrow={1}></Box>
+      </Flex>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
